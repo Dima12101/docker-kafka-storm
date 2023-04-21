2810412813be076c19a1faf8d289612553bce499
Терещенко Дмитрий Владиславович <tereshenko.dv@esphere.ru>
2023-04-21 18:19:24 +0300

    Исправления

--- show-storm-output.sh
+++ show-storm-output.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker exec -it supervisor tail -f /var/log/storm/worker-6702.log
+# docker exec -it supervisor tail -f /var/log/storm/worker-6702.log
+docker exec -it supervisor tail -f cat /logs/workers-artifacts/worker-6702.log
--- submit-storm-topology.sh
+++ submit-storm-topology.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
 docker cp WordCountTopology.jar nimbus:/WordCountTopology.jar
-docker exec -it nimbus \
-        storm jar /WordCountTopology.jar $1 $2 zookeeper 2181 $3
+case "$1" in
+        local)
+            docker exec -it nimbus storm jar /WordCountTopology.jar $2
+            ;;
+
+        kafka)
+            docker cp kafka-clients-2.8.1.jar nimbus:/apache-storm-2.4.0/lib/
+            docker exec -it nimbus \
+                storm jar /WordCountTopology.jar $2 $3 zookeeper 2181 $4 \
+                --jars "/apache-storm-2.4.0/lib/kafka-clients-2.8.1.jar"
+            ;;
+
+        *)
+            echo $"Usage: $0 {submit|kill}"
+            exit 1
+
+esac
